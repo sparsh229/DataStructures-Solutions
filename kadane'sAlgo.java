61517a2cf7fe48eb7376cc844351aac996fa8a30
Sparsh Gupta <sparsh.gupta229@gmail.com>
2020-12-01 16:54:52 +0530

    Create kadane'sAlgo.java

---
+++ kadane'sAlgo.java
@@ -0,0 +1,46 @@
+import java.io.*;
+
+class Array {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    //size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //adding elements
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Kadane obj = new Kadane();
+		    
+		    //calling maxSubarraySum() function
+		    System.out.println(obj.maxSubarraySum(arr, n));
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+class Kadane{
+    
+    // Function to find subarray with maximum sum
+    // arr: input array
+    // n: size of array
+    int maxSubarraySum(int arr[], int n){
+        int current_max=arr[0];
+        int global_max=arr[0];
+        for(int i=1;i<n;i++){
+            current_max=Math.max(current_max+arr[i],arr[i]);
+            global_max=Math.max(global_max,current_max);
+        }
+        return global_max;
+        
+    }
+    
+}
