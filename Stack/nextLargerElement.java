7cc2d13dad7b30b611dd59583dc96d748227228e
Sparsh Gupta <sparsh.gupta229@gmail.com>
2021-01-19 16:38:17 +0530

    Create nextLargerElement.java

---
+++ Stack/nextLargerElement.java
@@ -0,0 +1,57 @@
+/ { Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    int n = Integer.parseInt(br.readLine().trim());
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    long[] arr = new long[n];
+		    for(int i=0; i<n; i++)arr[i]=Long.parseLong(inputLine[i]);
+		    long[] res = new solve().nextLargerElement(arr, n);
+		    for (int i = 0; i < n; i++) 
+		        System.out.print(res[i] + " ");
+		    System.out.println();
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+class solve{
+    public static long[] nextLargerElement(long[] arr, int n) { 
+       Stack<Long>st=new Stack<>();
+       long []ans=new long[n];
+       for(int i=n-1;i>=0;i--){
+           if(st.size()==0){
+               ans[i]=-1;
+           }
+           else if(st.size()>0&&arr[i]<st.peek()){
+               ans[i]=st.peek();
+           }
+           else if(st.size()>0&&arr[i]>=st.peek()){
+               while(st.size()>0&&arr[i]>=st.peek()){
+                   st.pop();
+               }
+               if(st.size()==0){
+                   ans[i]=-1;
+               }
+               else{
+                   ans[i]=st.peek();
+               }
+           }
+           st.push(arr[i]);
+       }
+       return ans;
+    } 
+}
